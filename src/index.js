5d32b68780f08f2c67e07569eb15b77976e07722
Abraham <abrahamnhier97@gmail.com>
2024-03-25 17:20:51 +0300

    wrapped the app in google provider

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from "react-router-dom"
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+console.log("Google Client ID:", process.env.REACT_APP_GOOGLE_CLIENT_ID);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+    <GoogleOAuthProvider clientId={`${process.env.REACT_APP_GOOGLE_CLIENT_ID}`}>
     <App />
+    </GoogleOAuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
