3cb54343d295113b2ee3a9c17a2501aa5dda6ccf
Abraham <abrahamnhier97@gmail.com>
2024-03-26 11:26:29 +0300

    fetched user data in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,71 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from "react";
+import { HiMenu } from "react-icons/hi";
+import { AiFillCloseCircle } from "react-icons/ai";
+import { Link, Route, Routes } from "react-router-dom";
+import { SideBar, UserProfile } from "../components";
+import Pins from "./Pins";
+
+import { client } from "../client";
+import logo from "../assets/logo.png";
+import { userQuery } from "../utils/data"
 
 const Home = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [user, setUser] = useState(null);
+  const scrollRef = useRef(null);
+//get user info from localStorage
+const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
 
-export default Home
+//extract user info from sanity
+useEffect(() => {
+  
+  const query = userQuery(userInfo?._id);
+
+  client.fetch(query).then((data) => {
+    setUser(data[0]);
+  });
+}, [userInfo]);
+
+useEffect(() => {
+  scrollRef.current.scrollTo(0, 0);
+},[])
+
+  return (
+    <div className="flex bg-gray-50 md:flex-row flex-col h-screen transition-height duration-75 ease-out">
+    <div className="hidden md:flex h-screen flex-initial">
+      <SideBar user={user && user} />
+    </div>
+    <div className="flex md:hidden flex-row">
+      <div className="p-2 w-full flex flex-row justify-between items-center shadow-md">
+        <HiMenu fontSize={40} className="cursor-pointer" onClick={() => setIsSidebarOpen(true)} />
+        <Link to="/">
+          <img src={logo} alt="logo" className="w-28" />
+        </Link>
+        <Link to={`user-profile/${user?._id}`} onClick={() => console.log("User ID:", user?._id)}>
+          {user?.image ? (
+          <img src={user?.image} alt="user-pic" className="w-9 h-9 rounded-full " />
+          ) : (
+            <div className="w-9 h-9 rounded-full bg-gray-300"></div>
+          )}
+        </Link>
+      </div>
+      {isSidebarOpen && (
+      <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
+        <div className="absolute w-full flex justify-end items-center p-2">
+          <AiFillCloseCircle fontSize={30} className="cursor-pointer" onClick={() => setIsSidebarOpen(false)} />
+        </div>
+        <SideBar closeToggle={setIsSidebarOpen} user={user && user} />
+      </div>
+      )}
+    </div>
+    <div className="pb-2 flex-1 h-screen overflow-y-scroll" ref={scrollRef}>
+      <Routes>
+        <Route path="/user-profile/:userId" element={<UserProfile />} />
+        <Route path="/" element={<Pins user={user && user} />} />
+      </Routes>
+    </div>
+  </div>
+  )
+};
+
+export default Home;
